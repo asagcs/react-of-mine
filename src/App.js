2b236d005912e7266a6e437ab26420909313a2ba
lidongsheng <jj15838317072@163.com>
2021-05-13 18:01:44 +0800

    react路由

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+
+import React, { PureComponent } from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
-import { Button } from "antd"
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <Button title="测试" type="primary">测试</Button>
-    </div>
-  );
+import Home from './pages/home';
+import About from './pages/about';
+import Profile from './pages/profile';
+
+export default class App extends PureComponent {
+  render() {
+    return (
+      <BrowserRouter>
+        <Link to="/">首页</Link>
+        <Link to="/about">关于</Link>
+        <Link to="/profile">我的</Link>
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/profile" component={Profile} />
+      </BrowserRouter>
+    )
+  }
 }
-
-export default App;
